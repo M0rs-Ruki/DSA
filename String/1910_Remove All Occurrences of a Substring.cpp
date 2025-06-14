610ee0df89efd1bfe5bad333e45f98865d129e08
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-14 14:42:55 +0530

    1910. Remove All Occurrences of a Substring

---
+++ String/1910_Remove All Occurrences of a Substring.cpp
@@ -0,0 +1,19 @@
+
+// 1910. Remove All Occurrences of a Substring
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+public:
+    string removeOccurrences(string s, string part) {
+        
+        while (s.length() > 0 && s.find(part) < s.length()){
+            s.erase(s.find(part), part.length());   
+        }
+        return s;
+    }
+};
