c1b85ac044369b296ed2a95afd06ac37549e7d47
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-23 22:49:49 +0530

    Refactor Two Sum solution: update variable names and clean up commented code

--- Array/1_Two_Sum.cpp
+++ Array/1_Two_Sum.cpp
@@ -4,21 +4,39 @@
 #include <climits>
 using namespace std;
 
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+
+//         int n = nums.size();
+
+//         for(int i = 0; i < n; i++) {
+//             for(int j = i+1; j < n; j++) {
+//                 if(nums[i] + nums[j] == target) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+
+// Input: nums = [2,7,11,15], target = 9
+// Output: [0,1]
+
+
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-
-        int n = nums.size();
-
-        for(int i = 0; i < n; i++) {
-
+    vector<int> twoSum(vector<int>& arr, int target) {
+        int n = arr.size();
+        for (int i = 0; i < n; i++) {
             for(int j = i+1; j < n; j++) {
-
-                if(nums[i] + nums[j] == target) {
-                    return {i, j};
+                if(arr[i] + arr[j] == target) {
+                    return {i,j};
                 }
             }
         }
-        return {};
+        return{};
     }
 };
