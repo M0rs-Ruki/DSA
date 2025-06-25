4ca5fa5268182e8917a2e1df01e700ecb992b67c
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-25 23:46:28 +0530

    74. Search a 2D Matrix O(log(m * n))

--- 2D Arrays/74. Search a 2D Matrix.cpp
+++ 2D Arrays/74. Search a 2D Matrix.cpp
@@ -41,7 +41,39 @@ public:
 //  O(log(m * n))
 class Solution {
 public:
+    bool searchInRow(vector<vector<int>>& mat, int target, int row) {
+        int n = mat[0].size();
+        int st = 0, end = n - 1;
+        while (st <= end) {
+            int mid = st + (end - st) / 2;
+            if (target == mat[row][mid]) {
+                return true;
+            } else if (target > mat[row][mid]){
+                st = mid +1;
+            } else {
+                end = mid -1;
+            }
+        }
+        return false;
+    };
+
     bool searchMatrix(vector<vector<int>>& mat, int target) {
-        
+        int m = mat.size(), n = mat[0].size();
+        int startRow = 0, endRow = m -1;
+        while (startRow <= endRow ) {
+            int midRow = startRow + (endRow - startRow) / 2;
+
+            if(mat[midRow][0] <= target && target <= mat[midRow][n-1]) {
+                // find in middle row
+                return searchInRow(mat, target, midRow);
+            } else if (target >= mat[midRow][n-1]) {
+                // down  => Right
+                startRow = midRow +1;
+            } else {
+                // uper => left
+                endRow = midRow -1;
+            }
+        }
+        return false;
     }
 };
