93dbe09c740fa700ccaf53496835b714ca834334
Anup Pradhan <anuppradhan929@gmail.com>
2025-06-25 23:57:56 +0530

    240. Search a 2D Matrix II

---
+++ 2D Arrays/240. Search a 2D Matrix II.cpp
@@ -0,0 +1,26 @@
+
+// 240. Search a 2D Matrix II
+
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int m = mat.size(), n = mat[0].size();
+        int r = 0, c = n -1;
+        while(r < m && c >= 0) {
+            if(target == mat[r][c]) {
+                return true;
+            } else if(target < mat[r][c]) {
+                c--;
+            } else {
+                r++;
+            }
+        }
+        return false;
+    }
+};
