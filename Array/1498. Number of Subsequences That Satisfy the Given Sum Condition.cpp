21b62166af9cf0cd54282a03014bbc299e840f45
Anup Pradhan <anuppradhan929@gmail.com>
2025-07-10 10:42:26 +0530

    Add initial implementation for 1498. Number of Subsequences That Satisfy the Given Sum Condition and 91. Decode Ways

---
+++ Array/1498. Number of Subsequences That Satisfy the Given Sum Condition.cpp
@@ -0,0 +1,16 @@
+
+
+// 1498. Number of Subsequences That Satisfy the Given Sum Condition
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int numSubseq(vector<int>& nums, int target) {
+        
+    }
+};
---
+++ String/91. Decode Ways.cpp
@@ -0,0 +1,22 @@
+
+// 91. Decode Ways
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+class Solution {
+public:
+    int numDecodings(string s) {
+        int n = s.size();
+        int ans = 0;
+        int i = 65;
+        while ( i <= 90 ) {
+            cout << i << " ";
+            i++;
+        }
+        return 0;
+    }
+};
