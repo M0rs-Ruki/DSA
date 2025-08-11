e082548f2b5862c78ca5d90661cc068eff3f38c1
Anup Pradhan <anuppradhan929@gmail.com>
2025-08-12 00:38:31 +0530

    Revision

--- Revision/1. Two Sum.cpp
+++ Revision/1. Two Sum.cpp
@@ -3,20 +3,35 @@
 #include <vector>
 #include <algorithm>
 #include <climits>
+#include <unordered_map>
 using namespace std;
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& arr, int target) {
+//         int n = arr.size();
+
+//         for (int i = 0; i < n; i++) {
+//             for(int j = i+1; j < n; j++ ) {
+//                 if (arr[i] + arr[j] == target) {
+//                     return {i, j};
+//                 }
+//             }
+//         }
+//         return {};
+//     }
+// };
+
+
+
+// 2 pointer
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& arr, int target) {
-        int n = arr.size();
 
-        for (int i = 0; i < n; i++) {
-            for(int j = i+1; j < n; j++ ) {
-                if (arr[i] + arr[j] == target) {
-                    return {i, j};
-                }
-            }
-        }
-        return {};
+        vector<pair<int, int>> orig;
+        
+
     }
 };
---
+++ Revision/11. Container With Most Water.cpp
@@ -0,0 +1,56 @@
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+#include <unordered_map>
+using namespace std;
+
+// class Solution {
+// public:
+//     int maxArea(vector<int>& arr) {
+        
+//         int maxWater = 0;
+//         int n = arr.size();
+
+//         for (int i = 0; i < n; i++) {
+//             for (int j = i+i; j < n; j++) {
+//                 int w = j-1;
+//                 int h = min(arr[i] , arr[j]);
+//                 int currWater = w * h;
+
+//                 maxWater = max(maxWater, currWater);
+
+//             }
+            
+//         }
+//         return maxWater;
+//     }
+// };
+
+
+// Two Pointer Approach
+
+class Solution {
+public:
+    int maxArea(vector<int>& arr) {
+        int right = 0;
+        int left = arr.size()-1;
+        int maxWa = 0;
+
+        while (left < right) {
+            int w = right - left;
+            int h = min(arr[left], arr[right]);
+            maxWa = max(maxWa, w * h);
+
+            if (arr[left] < arr[right]) {
+                left++;
+            } else {
+                right--;
+            }
+
+            // arr[left] < arr[right] ? left++ : right--;
+        }
+        return maxWa;
+    }
+};
