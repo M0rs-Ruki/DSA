373792fb49de2bbb09b4d16d968a1c0313650c4f
Anup Pradhan <anuppradhan929@gmail.com>
2025-08-14 22:07:18 +0530

    Revision

--- Revision/35. Search Insert Position.cpp
+++ Revision/35. Search Insert Position.cpp
@@ -21,3 +21,26 @@ public:
         return n;
     }
 };
+
+
+// Binary Search
+
+class Solution {
+public:
+    int searchInsert(vector<int>& arr, int target) {
+        int start = 0, end = arr.size() - 1;
+        
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            if (arr[mid] == target) {
+                return mid;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        
+        return start;
+    }
+};
