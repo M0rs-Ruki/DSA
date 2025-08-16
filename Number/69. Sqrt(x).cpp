c9ead58fb1916a8da2131d077fe7c0e717da4973
Anup Pradhan <anuppradhan929@gmail.com>
2025-08-16 23:50:33 +0530

    69. Sqrt(x) // Binary Search

--- Number/69. Sqrt(x).cpp
+++ Number/69. Sqrt(x).cpp
@@ -26,7 +26,24 @@ public:
 class Solution {
 public:
     int mySqrt(int x) {
-        
+        int start = 0;
+        int end = x;
+        int ans =0;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+            long long sq = (long long)mid * mid;
+            if (sq == x) {
+                ans = mid;
+                break;
+            } else if (sq < x) {
+                ans = mid;
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            };
+        };
+        return ans;
     }
 };
 
