dab1a273e8be25d0ff3f13ddb99afd268fe8af75
Anup Pradhan <anuppradhan929@gmail.com>
2025-08-20 12:26:35 +0530

    Revision

--- Revision/2965. Find Missing and Repeated Values.cpp
+++ Revision/2965. Find Missing and Repeated Values.cpp
@@ -11,7 +11,27 @@ using namespace std;
 
 class Solution {
 public:
-    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
-        
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& arr) {
+        vector<int> ans;
+        unordered_set<int> set;
+        int n = arr.size();
+        int a,b;
+        int expSum = 0, actualSum = 0 ;
+
+        for (int i=0; i<n; i++) {
+            for (int j=0; j<n; j++) {
+                actualSum += arr[i][j];
+                if (set.find(arr[i][j]) != set.end()) {
+                    a = arr[i][j];
+                    ans.push_back(a);
+
+                }
+                set.insert(arr[i][j]);
+            }
+        }
+        expSum = (n*n) * (n*n + 1) / 2;
+        b = expSum + a - actualSum;
+        ans.push_back(b);
+        return ans; 
     }
 };
